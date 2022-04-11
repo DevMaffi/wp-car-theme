77d97346b5430808ecfef00c0efb6332683cb391
devmaffi <devmaffi550@gmail.com>
2022-04-11 10:52:11 +0300

    Break index.php layout into sections

--- footer.php
+++ footer.php
@@ -7,3 +7,111 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
+
+?>
+
+<!-- FOOTER -->
+<footer class="footer section" data-blur-target>
+  <div class="shape shape__big"></div>
+  <div class="shape shape__small"></div>
+
+  <!-- FOOTER CONTAINER -->
+  <div class="footer__container container grid">
+
+    <!-- FOOTER CONTENT -->
+    <div class="footer__content">
+      <a class="footer__logo" href="#">
+        <i class="ri-steering-line"></i>
+        Elecar
+      </a>
+      <p class="footer__description">
+        We offer the best electric cars of
+        <br />
+        the most recognized brands in
+        <br />
+        the world.
+      </p>
+    </div>
+
+    <!-- FOOTER CONTENT -->
+    <div class="footer__content">
+      <h3 class="footer__title">Company</h3>
+
+      <!-- FOOTER LINKS -->
+      <ul class="footer__links">
+        <li>
+          <a class="footer__link" href="#">About</a>
+        </li>
+        <li>
+          <a class="footer__link" href="#">Cars</a>
+        </li>
+        <li>
+          <a class="footer__link" href="#">History</a>
+        </li>
+        <li>
+          <a class="footer__link" href="#">Shop</a>
+        </li>
+      </ul>
+    </div>
+
+    <!-- FOOTER CONTENT -->
+    <div class="footer__content">
+      <h3 class="footer__title">Information</h3>
+
+      <!-- FOOTER LINKS -->
+      <ul class="footer__links">
+        <li>
+          <a class="footer__link" href="#">Request a quote</a>
+        </li>
+        <li>
+          <a class="footer__link" href="#">Find a dealer</a>
+        </li>
+        <li>
+          <a class="footer__link" href="#">Contact us</a>
+        </li>
+        <li>
+          <a class="footer__link" href="#">Services</a>
+        </li>
+      </ul>
+    </div>
+
+    <!-- FOOTER CONTENT -->
+    <div class="footer__content">
+      <h3 class="footer__title">Follow us</h3>
+
+      <!-- FOOTER SOCIAL -->
+      <ul class="footer__social">
+        <a class="footer__social-link" href="https://www.facebook.com/" target="_blank">
+          <i class="ri-facebook-fill"></i>
+        </a>
+        <a class="footer__social-link" href="https://www.instagram.com/" target="_blank">
+          <i class="ri-instagram-line"></i>
+        </a>
+        <a class="footer__social-link" href="https://twitter.com/" target="_blank">
+          <i class="ri-twitter-line"></i>
+        </a>
+      </ul>
+    </div>
+  </div>
+  <span class="footer__copy">© 2022 Elecar. All rights reserved</span>
+</footer>
+</main>
+
+<!-- SCROLL UP -->
+<a class="scroll-up" id="scroll-up" href="#">
+  <i class="ri-arrow-up-line"></i>
+</a>
+
+<!-- svg element with blur filter applied -->
+<svg class="hide-svg" id="svg-image-blur">
+  <filter id="blur-effect">
+    <feGaussianBlur stdDeviation="86"></feGaussianBlur>
+  </filter>
+</svg>
+
+<?php
+wp_footer();
+?>
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -7,3 +7,66 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- HEAD -->
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="shortcut icon" href="<?= bloginfo('template_url'); ?>/assets/images/favicon.png" type="image/x-icon" />
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
+  <title>Responsive Car Website</title>
+
+  <?php
+  wp_head();
+  ?>
+</head>
+
+<!-- BODY -->
+
+<body>
+
+  <!-- MAIN -->
+  <main class="main">
+
+    <!-- HEADER -->
+    <header class="header" id="header" data-blur-target>
+
+      <!-- NAV CONTAINER -->
+      <nav class="nav container">
+        <a class="nav__logo" href="#">
+          <i class="ri-steering-line"></i>
+          Elecar
+        </a>
+
+        <!-- NAV MENU -->
+        <div class="nav__menu" id="nav-menu">
+          <ul class="nav__list">
+            <li class="nav__item">
+              <a class="nav__link active-link" href="#home">Home</a>
+            </li>
+            <li class="nav__item">
+              <a class="nav__link " href="#about">About</a>
+            </li>
+            <li class="nav__item">
+              <a class="nav__link " href="#popular">Popular</a>
+            </li>
+            <li class="nav__item">
+              <a class="nav__link " href="#featured">Featured</a>
+            </li>
+          </ul>
+          <div class="nav__close" id="nav-close">
+            <i class="ri-close-line"></i>
+          </div>
+        </div>
+        <div class="nav__toggle" id="nav-toggle">
+          <i class="ri-menu-line"></i>
+        </div>
+      </nav>
+    </header>
